d5a415677d8b2cf1ead7b11497d5c43ffb7cdd0c
francosorbello <franco.sorbelo@gmail.com>
2025-09-25 11:51:59 -0300

    fixes to bubble based on rigidbody

--- bubble/bubble_rb.gd
+++ bubble/bubble_rb.gd
@@ -1,22 +1,12 @@
 extends RigidBody2D
 
-@export var mu_static = 0.8  # friction coefficients
-@export var mu_moving = 0.5  # pushing something moving is easier
-# mu depends on what material the object is on, so use area detectors and change
-# this values depending on ice or mud
+@export var start_force : float = 200
 
-@export var move_strength = 50
+signal popped
 
-var applied_forces: Vector2 = Vector2(0, 0)
+func start(dir : Vector2):
+	apply_central_impulse(dir * start_force)
 
-func add_force_for_frame(force: Vector2):
-	# add_force adds a permanent force, for a temporary one we need to wipe it
-	# by undo the force next frame, just keep track of forces applied
-	applied_forces += force
-	self.apply_central_force(force)
-
-func _ready() -> void:
-	# no world gravity pushing the object down (in the +y) direction
-	# we are top down so gravity is acting into the screen (in +z) but the
-	# "ground" normal force is canceling it out
-	self.gravity_scale = 0
+func _on_dead_timer_timeout() -> void:
+	popped.emit(self)
+	pass # Replace with function body.

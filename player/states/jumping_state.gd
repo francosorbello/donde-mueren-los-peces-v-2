84eebc938c85f95738750538e85ed14610ffc136
francosorbello <franco.sorbelo@gmail.com>
2025-10-05 12:04:08 -0300

    added licences addon

--- player/states/jumping_state.gd
+++ player/states/jumping_state.gd
@@ -2,10 +2,13 @@ extends PlayerState
 
 @export var move_speed : float = 40
 @export_range(1,25) var decay : float = 5
+
 func enter():
+    $JumpDurationTimer.start()
     player.play_anim("bubble_up")
 
 func exit():
+    $JumpDurationTimer.stop()
     player.play_anim("bubble_down")
 
 func state_unhandled_input(event : InputEvent):
@@ -23,8 +26,9 @@ func physics_update(delta: float):
     player.velocity = FreyaMath.lerp_exp_decay(player.velocity,direction * move_speed, decay, delta)
     player.move_and_slide()
 
-    if direction == Vector2.ZERO:
-        # state_machine.transition_to("IdleState")
-        return
-    else:
+    if direction != Vector2.ZERO:
         player.last_direction = direction
+
+func _on_jump_duration_timer_timeout() -> void:
+    state_machine.transition_to("MovingState")
+    pass # Replace with function body.

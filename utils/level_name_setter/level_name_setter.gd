e1efe68d2996a4f3508472defd84c3789d4f8425
francosorbello <franco.sorbelo@gmail.com>
2025-10-24 13:14:04 -0300

    added tool to quickly set variables related to a levels name

--- utils/level_name_setter/level_name_setter.gd
+++ utils/level_name_setter/level_name_setter.gd
@@ -3,9 +3,18 @@ extends Node
 
 @export var level_name : String
 @export_tool_button("Set level name") var set_level_name_action = _set_name
+@export_tool_button("Set level name from scene path") var set_name_from_path_action = _set_name_from_file
+
+func _set_name_from_file():
+    level_name = SaveUtils.scene_path_to_name(owner.scene_file_path) 
+    _set_name()
 
 func _set_name():
-    var scene_root = get_tree().root
+
+    if level_name.is_empty():
+        return
+
+    var scene_root = owner
     scene_root.name = level_name
     var level_info : LevelInfo = scene_root.find_child("LevelInfo")
     if level_info:
--- utils/save_utils.gd
+++ utils/save_utils.gd
@@ -7,3 +7,9 @@ static func get_save() -> ASavedGame:
         return saved_game
     
     return null
+
+static func scene_path_to_name(path : String):
+    var sliced_path = path.split("/")
+    var file_name_tscn = sliced_path[sliced_path.size()-1]
+    var name_only = file_name_tscn.get_slice(".tscn",0)
+    return name_only     

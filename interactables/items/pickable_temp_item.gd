4c1531f8630b7e245dad8595916929db340d3016
francosorbello <franco.sorbelo@gmail.com>
2025-10-27 20:53:35 -0300

    added warnings to pickable temp item

--- interactables/items/pickable_temp_item.gd
+++ interactables/items/pickable_temp_item.gd
@@ -1,12 +1,40 @@
+@tool
 extends Node2D
 
 var evaluator : PersistentEventEvaluator 
 
 var event_setter : PersistentEventSetter
 
-@export var event_setters : Array[EventSetterResource]
+@export var event_setters : Array[EventSetterResource]:
+    set(value):
+        event_setters = value
+        update_configuration_warnings()
+
+func _get_configuration_warnings() -> PackedStringArray:
+    var warnings : PackedStringArray = []
+    if event_setters.is_empty():
+        warnings.append("No event setters available")
+
+    var has_evaluator : bool = false
+    var has_interactable : bool = false
+    for child in get_children():
+        if child.has_signal("interacted_with_item"):
+            has_interactable = true
+        if child is PersistentEventEvaluator:
+            has_evaluator = true
+
+    if not has_evaluator:
+        warnings.append("PersistentEventEvaluator child missing")
+    
+    if not has_interactable:
+        warnings.append("InteractableItem child missing")
+
+    return warnings
 
 func _ready() -> void:
+    if Engine.is_editor_hint():
+        return
+    
     for child in get_children():
         if child.has_signal("interacted_with_item"):
             child.interacted_with_item.connect(_on_interactable_item_interacted_with_item)

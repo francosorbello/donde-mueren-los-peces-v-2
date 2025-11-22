4500e01fefab5209e7045837441a9a9491879bf0
francosorbello <franco.sorbelo@gmail.com>
2025-11-22 13:57:31 -0300

    added timed levers

--- interactables/levers/room_lever.gd
+++ interactables/levers/room_lever.gd
@@ -1,6 +1,11 @@
 @tool
 extends Node2D
 
+@export var timed : bool = false
+@export var timed_duration : float = 5
+
+var activated : bool = false
+
 var action : RoomAction:
     set(value):
         action = value
@@ -26,6 +31,31 @@ func _ready() -> void:
 func _on_better_interactable_component_on_interact() -> void:
     if action:
         action.use()
+        toggle_activated()
+        if timed:
+            $Timer.start(timed_duration)
+            $ProgressBar2D.start(timed_duration)
 
 func _on_child_changed(_node):
     update_configuration_warnings()
+
+func flip_sprite():
+    $Sprite2D.flip_h = true
+
+func set_activated():
+    $Sprite2D.flip_h = not $Sprite2D.flip_h
+    $BetterInteractableComponent.set_deferred("is_interactable",false)
+    $LeverSound.play()
+
+func toggle_activated():
+    activated = not activated
+
+    $Sprite2D.flip_h = activated
+    if timed:
+        $BetterInteractableComponent.set_deferred("is_interactable",not activated)
+    $LeverSound.play()
+
+func _on_timer_timeout() -> void:
+    if action:
+        action.use()
+    toggle_activated()
---
+++ utils/progress_bar_2d/progress_bar_2d.gd
@@ -0,0 +1,65 @@
+@tool
+extends Node2D
+
+@export_range(0,1,0.01) var progress_percent : float = 0:
+    set(value):
+        progress_percent = value
+        queue_redraw()
+
+@export var radius : float = 10:
+    set(value):
+        radius = value
+        queue_redraw()
+
+@export var width : float = 10:
+    set(value):
+        width = value
+        queue_redraw()
+
+@export var color_overrides : Dictionary[float,Color]
+
+var duration : float = 0
+var _acc_time : float = 0
+var running : bool = false
+
+func start(time : float):
+    if time <= 0:
+        push_error("Trying to start a progress bar 2D with a time of %f (must be higher than 0)")
+        return
+
+    if running:
+        return
+
+    progress_percent = 0
+    duration = time
+    _acc_time = 0
+    running = true
+
+func _process(delta):
+    if not running:
+        return
+
+    _acc_time += delta
+    if _acc_time > duration:
+        stop()
+    queue_redraw()    
+
+func stop():
+    running = false
+    progress_percent = 0
+    _acc_time = 0
+
+func _test():
+    start(5)
+
+func _draw() -> void:
+    var progress = progress_percent
+    if running:
+        progress = _acc_time / duration
+
+    var color = Color.WHITE
+    for key in color_overrides.keys():
+        if key < progress:
+            color = color_overrides[key]
+    
+    draw_arc(Vector2.ZERO,radius,0,progress * 2 * PI,20,color,width)

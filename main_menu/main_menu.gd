a5de9b64d118721ccb4b5e1b2b9c1f07132f6b79
francosorbello <franco.sorbelo@gmail.com>
2025-12-12 10:27:38 -0300

    added kiosk mode

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,6 +1,13 @@
 extends Control
 
+@export var kiosk_mode : bool = false
+@export var initial_level : StringResource
+
 func _on_start_button_pressed() -> void:
+    if kiosk_mode:
+        do_kiosk_mode()
+        return
+
     if IndieBlueprintSaveManager.save_filename_exists("test"):
         var saved_game = IndieBlueprintSaveManager.load_savegame("test")
         if saved_game is not ASavedGame:
@@ -22,6 +29,18 @@ func create_test_game():
     IndieBlueprintSaveManager.make_current(new_game)
     print("Created test savegame")
 
+func do_kiosk_mode():
+    if IndieBlueprintSaveManager.save_filename_exists("test"):
+        _on_reset_save_button_pressed()
+    else:
+        create_test_game()
+    initial_level.value = "lvl_vs_room_1"
+    ResourceSaver.save(initial_level,initial_level.resource_path)
+
+    get_parent().start_game()
+
+    
+    
 
 func _on_reset_save_button_pressed() -> void:
     var save_game = IndieBlueprintSaveManager.load_savegame("test") as ASavedGame

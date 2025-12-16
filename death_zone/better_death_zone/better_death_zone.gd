90ce4eb4cb329613b282910f1d46d46df920eeaa
francosorbello <franco.sorbelo@gmail.com>
2025-12-16 19:44:09 -0300

    added particles to death zones

--- death_zone/better_death_zone/better_death_zone.gd
+++ death_zone/better_death_zone/better_death_zone.gd
@@ -5,6 +5,9 @@ class_name BetterDeathZone
 @export_flags_2d_physics var collision_layer : int
 @export var polygon_material : ShaderMaterial
 @export var polygon_texture : Texture2D
+@export var particles_scene : PackedScene
+
+var particles : CPUParticles2D
 
 func spawn_required_children():
     super()
@@ -20,6 +23,19 @@ func spawn_required_children():
         if polygon_material:
             polygon_shape.material = polygon_material
 
+    particles = particles_scene.instantiate()
+    add_child(particles)
+    particles.name += str(randi_range(0,1000))
+    particles.owner = get_tree().edited_scene_root
+
+func create():
+    super()
+    var poly_rect = get_polygon_rect(polygon_shape.polygon)
+    prints(poly_rect,poly_rect.get_center())
+    particles.position = poly_rect.get_center()
+    particles.emission_rect_extents = poly_rect.size/2 + Vector2(10,10)
+
+
 func set_polygon_shape_uvs():
     var tex_size = polygon_shape.texture.get_size()
     var p0 = Vector2(0,tex_size.y)
@@ -28,3 +44,21 @@ func set_polygon_shape_uvs():
     var p3 = Vector2.ZERO
 
     polygon_shape.uv = [p0,p1,p2,p3]
+
+func get_polygon_rect(points: PackedVector2Array) -> Rect2:
+    var rect = Rect2()
+    if points.is_empty():
+        return rect
+
+    var min_point = points[0]
+    var max_point = points[1]
+    for point in points:
+        if point.x < min_point.x or point.y < min_point.y:
+            min_point = point
+        if point.x > max_point.x or point.y > max_point.y:
+            max_point = point
+        # rect = rect.expaAnd(point)
+    
+    rect.position = min_point
+    rect.end = max_point
+    return rect

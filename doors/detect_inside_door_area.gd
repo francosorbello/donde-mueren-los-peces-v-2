3b36fe48a137d7cceb533bf5de7ca9c44a61dab5
francosorbello <franco.sorbelo@gmail.com>
2025-12-19 18:22:15 -0300

    connected levels 13 trough 15

--- doors/detect_inside_door_area.gd
+++ doors/detect_inside_door_area.gd
@@ -3,7 +3,6 @@ extends Area2D
 var running : bool = false
 
 func start():
-    print("Starting player detection")
     running = true
     _player = null
     
@@ -13,7 +12,6 @@ var _player : APlayer
 
 func _process(_delta):
     if running:
-        print("Has player: ",_player != null)
         var bodies = get_overlapping_bodies()
         for body in bodies:
             if body is APlayer:
@@ -22,7 +20,6 @@ func _process(_delta):
         _player = null
 
 func _on_running_timer_timeout() -> void:
-    print("Stopping player detection. Has player?",_player != null)
     running = false
     if _player:
         _player.die()

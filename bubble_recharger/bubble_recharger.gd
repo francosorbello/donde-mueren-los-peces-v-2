3d85f57d7529812937076fab4e0ea50e35784e89
francosorbello <franco.sorbelo@gmail.com>
2025-12-19 19:26:07 -0300

    bubble rechargers can be toggled now

--- bubble_recharger/bubble_recharger.gd
+++ bubble_recharger/bubble_recharger.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var disabled : bool = false
+
 func _ready():
 	$AnimatedSprite2D.play()
 
@@ -11,5 +13,19 @@ func _draw() -> void:
 	draw_circle(Vector2.ZERO,7,Color(1,1,1,0.45))
 
 func do_toggle():
-	visible = not visible
-	
+	disabled = not disabled
+	$Area2D/CollisionShape2D.disabled = disabled
+	if not disabled:
+		$UnlockSound.play()
+	do_toggle_anim()
+
+func do_toggle_anim():
+	var tween := create_tween()
+	var anim_to : Vector2
+
+	if disabled:
+		anim_to = Vector2.ZERO
+	else:
+		anim_to = Vector2.ONE
+
+	tween.tween_property(self,"scale",anim_to,0.5)

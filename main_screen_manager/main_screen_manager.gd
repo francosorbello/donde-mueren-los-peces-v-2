785ee61a94d997d5dc0c6c888d62b17245a099ba
francosorbello <franco.sorbelo@gmail.com>
2025-12-23 18:56:56 -0300

    removed individual references to main scene and game scene on main screen manager. unified system under a single dictionary

--- main_screen_manager/main_screen_manager.gd
+++ main_screen_manager/main_screen_manager.gd
@@ -1,9 +1,7 @@
 extends Node
+class_name MainScreenManager
 ## Autoload that handles main screen
 ## The game is separated in a series of screens, like the menu or the game itself
-
-@export var main_menu_scene : PackedScene
-@export var game_scene : PackedScene
 
 @export var screens : Dictionary[String,PackedScene]
 
@@ -18,11 +16,7 @@ func _ready():
     GlobalSignal.on_request_main_scene_change.connect(_main_scene_change_requested)
 
 
-func _main_scene_change_requested(new_scene : String):
-    if new_scene == "MainMenu":
-        start_main_menu()
-        return
-    
+func _main_scene_change_requested(new_scene : String):    
     if screens.has(new_scene):
         _start_scene.call_deferred(screens[new_scene])
     else:
@@ -48,10 +42,10 @@ func _clear_children():
         child.queue_free()
 
 func start_game():
-    _start_scene(game_scene)
+    _start_scene(screens["Game"])
 
 func start_main_menu():
-    _start_scene(main_menu_scene)
+    _start_scene(screens["MainMenu"])
 
 func start_intro_screen():
     _start_scene(screens["IntroScreen"])

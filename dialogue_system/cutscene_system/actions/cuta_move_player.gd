99cc13a9e8a814c7a400b614003dc5cfc3a21969
francosorbello <franco.sorbelo@gmail.com>
2026-01-11 21:05:15 -0300

    misc fixes

--- dialogue_system/cutscene_system/actions/cuta_move_player.gd
+++ dialogue_system/cutscene_system/actions/cuta_move_player.gd
@@ -5,11 +5,11 @@ extends CutsceneAction
 var player : APlayer
 
 func do_action():
-    if execute == Execute.ON_START:
-        await get_tree().process_frame
-    player = get_tree().get_first_node_in_group("player")
-    
-    assert(player != null, "(%s) No player to move"%name)
-    assert(target != null, "(%s) No target to move the player to"%name)
-    
-    player.move_to(target)
+	if execute == Execute.ON_START:
+		await get_tree().process_frame
+	player = get_tree().get_first_node_in_group("player")
+	
+	assert(player != null, "(%s) No player to move"%name)
+	assert(target != null, "(%s) No target to move the player to"%name)
+	
+	player.move_to(target)

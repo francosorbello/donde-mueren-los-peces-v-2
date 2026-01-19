ba496146e10c556350ac4881067b8e1106444817
francosorbello <franco.sorbelo@gmail.com>
2026-01-19 12:08:07 -0300

    added basic settings menu with window mode and volume sliders

---
+++ settings_menu/settings_menu.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _ready():
+    $TabContainer/Game.grab_focus()
---
+++ settings_menu/volume_slider.gd
@@ -0,0 +1,12 @@
+extends HSlider
+
+@export var bus_name : String
+
+var bus_id : int
+
+func _ready():
+    bus_id = AudioServer.get_bus_index(bus_name)
+    set_value_no_signal(AudioServer.get_bus_volume_linear(bus_id))
+
+func _on_value_changed(_value: float) -> void:
+    AudioServer.set_bus_volume_db(bus_id, linear_to_db(_value))

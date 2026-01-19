6c03bf0c7139775c377cc4ac98d6c4f4776946d0
francosorbello <franco.sorbelo@gmail.com>
2026-01-19 19:31:50 -0300

    finished input system

--- game/ui/keyboard_controls.gd
+++ game/ui/keyboard_controls.gd
@@ -4,7 +4,6 @@ extends Control
 @export var dash_ability : AnItem
 
 func _ready() -> void:
-    print("Hello keyboard controls")
     InputHelper.device_changed.connect(_on_input_device_changed)
     _on_input_device_changed(InputHelper.guess_device_name(),0)
     var save = SaveUtils.get_save()
@@ -23,7 +22,6 @@ func handle_new_ability(_ab : AnItem):
 
 
 func _on_input_device_changed(device : String, _index : int):
-    print(device)
     if device == InputHelper.DEVICE_KEYBOARD:
         show()
     else:
